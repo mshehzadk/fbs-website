c4657af53d0ff1cfc752c0d3a5dbe7cb444075f4
Muhammad Shehzad <mshehzadmshehzad113440@gmail.com>
2023-02-11 14:03:31 +0500

    Initailized Project and Added Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
-
+import ChakraWrapper from "@/components/ChakraWrapper";
+import Navbar from "@/navbar";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraWrapper>
+          <Navbar />
+          {children}
+        </ChakraWrapper>
+      </body>
     </html>
-  )
+  );
 }
