72a28d4149f79ff91adc76bea1df330c950ae998
Muhammad Shehzad <mshehzadmshehzad113440@gmail.com>
2023-03-10 09:27:31 +0500

    updated key

--- src/components/faq/Faq.tsx
+++ src/components/faq/Faq.tsx
@@ -14,11 +14,13 @@ import { Faq_Items } from "./Faq_Item";
 
 const items = [
   {
+    key: 0,
     Question: "How does the Fast Blcokchain Society Work?",
     Answer:
       "Blockchain technology presents unique benefits of strong security encryptions, unchangeable data and a decentralized platform. With the onset of a pandemic, businesses must adapt to a secured and reliable technological system. This is where Blockchain Technology is seen as the most reliable technology to look forward to and rely upon. \n Fast Blcokchain Society is an local society of Fast that focuses on bringing Blockchain technology in traditional trust business. The community enables the business owners to interact with the tech-savvy blockchain enthusiast. This enables traditional businesses to get acquainted with, also equip themselves with the right technology for their business growth.",
   },
   {
+    key:1,
     Question: "How can I join Fast Blockchain Society?",
     Answer:
       "Fast Blockchain community is group of blockchain enthusiats & borderless community in Pakistan. It provides a unique opportunity for Students and partners to interact and support each other. It allows all the individuals involved to make the maximum utilization of its close network comprising of technology-savvy experts and individuals with an entrepreneurial mindset. \n So, don't wait come and join us to make the world more trust worthy.",
@@ -43,7 +45,7 @@ export default function Faq() {
             <Accordion allowToggle>
               <>
                 {items.map((item) => (
-                  <Faq_Items Questions={item.Question} Answer={item.Answer} />
+                  <Faq_Items Questions={item.Question} Answer={item.Answer} key={item.key}/>
                 ))}
               </>
             </Accordion>
