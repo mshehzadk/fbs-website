a01c3795f64ed76095760356cf4b19f2f7cd9edd
Zahid Imran <zahidimran479833@gmail.com>
2023-04-01 01:46:24 +0500

    feature-card updated

--- src/components/features/Feature_Card.tsx
+++ src/components/features/Feature_Card.tsx
@@ -1,6 +1,6 @@
 import { Box, Heading, HStack, Text } from "@chakra-ui/react";
 import React from "react";
-
+//Card for Features of Blockchain Soceity 
 export const Feature_Card = ({ iconComponent, title, txt }: any) => {
   return (
     <>
